3ed4e55d2ae81c6ee335bc0dcb3009e0c99056c5
GuilhermeC3Dev <guilhermecosta030409@gmail.com>
2025-03-21 17:12:50 -0300

    Add files via upload

---
+++ POO/MPL bibloteca Math, Java/AtivDistPoints.java
@@ -0,0 +1,20 @@
+
+
+public class AtivDistPoints {
+    public static void main(String[] args) {
+        calcDist();
+    }
+
+    public static void calcDist () {
+        String[] tupla0 = new String[]{"primeiro", "segundo"};
+        String[] tupla1 = new String[]{"primeiro", "segundo"};
+        String[] tupla2 = new String[]{"primeiro", "segundo"};
+        String[] tupla3 = new String[]{"primeiro", "segundo"};
+        String[] tupla4 = new String[]{"primeiro", "segundo"};
+        String[] tupla5 = new String[]{"primeiro", "segundo"};
+        String[] tupla6 = new String[]{"primeiro", "segundo"};
+        String[] tupla7 = new String[]{"primeiro", "segundo"};
+        String[] tupla8 = new String[]{"primeiro", "segundo"};
+        String[] tupla9 = new String[]{"primeiro", "segundo"};
+    }
+}
